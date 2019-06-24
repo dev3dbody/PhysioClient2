ace9d7cc568eab8d937a4521dafc6121487b5fc3
jaaaco <jakub@wietrzyk.com>
2019-06-24 08:53:14 +0200

    simple main layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import Switch from "./components/Switch";
 import Navigation from "./components/Navigation";
 
 const style = {
-  display: "flex" /* establish flex container */,
-  flexDirection: "column" /* make main axis vertical */,
-  justifyContent: "center" /* center items vertically, in this case */,
-  alignItems: "center" /* center items horizontally, in this case */,
-  height: "90vmin"
+  margin: "2em"
 } as CSSProperties;
 
 const App: React.FunctionComponent = () => (
