f510acc647c3e969b27c0b814dc86e8741185900
jaaaco <jakub@wietrzyk.com>
2019-07-03 21:48:26 +0200

    boilerplate do testow e2e + integracja z circle ci

--- e2e/index.js
+++ e2e/index.js
@@ -1,18 +1,48 @@
-import { beforeAll, describe, it } from "mocha";
+require("@babel/polyfill");
+
+import { beforeEach, afterEach, describe, it } from "mocha";
 import { Application } from "spectron";
 import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
 
 chai.should();
+chai.use(chaiAsPromised);
 
-describe("e2e tests", () => {
-  let app;
-  beforeAll(async () => {
-    app = new Application({
-      path: "/Applications/MyApp.app/Contents/MacOS/MyApp"
+describe("e2e tests", function() {
+  this.timeout(10000);
+
+  beforeEach(function() {
+    this.app = new Application({
+      path: "./dist/mac/Physio Client 2.app/Contents/MacOS/Physio Client 2"
     });
-    await app.start();
+    return this.app.start();
   });
-  it("window is visible", () => {
-    app.browserWindow.isVisible().should.be.true();
+
+  beforeEach(function() {
+    chaiAsPromised.transferPromiseness = this.app.transferPromiseness;
   });
+
+  afterEach(function() {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop();
+    }
+  });
+
+  it("opens app window", function() {
+    return this.app.client
+      .waitUntilWindowLoaded()
+      .getWindowCount()
+      .should.eventually.have.at.least(1)
+      .browserWindow.isMinimized()
+      .should.eventually.be.false.browserWindow.isVisible()
+      .should.eventually.be.true.browserWindow.isFocused()
+      .should.eventually.be.true.browserWindow.getBounds()
+      .should.eventually.have.property("width")
+      .and.be.above(0)
+      .browserWindow.getBounds()
+      .should.eventually.have.property("height")
+      .and.be.above(0);
+  });
+
+  require("./navigation");
 });
