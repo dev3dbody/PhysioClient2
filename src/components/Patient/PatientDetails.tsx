64795fa90da84144e420b2f7dbc7c3bfce39d4be
kamyk238 <kama.violinist@gmail.com>
2019-07-10 23:42:41 +0200

    Tablica Ostatnie wizyty dodana + style

--- src/components/Patient/PatientDetails.tsx
+++ src/components/Patient/PatientDetails.tsx
@@ -7,7 +7,9 @@ import {
   Dropdown,
   Header,
   Icon,
-  Segment
+  Segment,
+  Image,
+  Table
 } from "semantic-ui-react";
 import { getCurrentPatient } from "../../redux/reducers";
 import { edit, navigate } from "../../redux/actions";
@@ -34,19 +36,13 @@ const PatientDetails: React.FunctionComponent<{}> = () => {
             </Header>
           </Grid.Column>
           <Grid.Column>
-            <Button style={{ margin: "2px" }}>Dalej</Button>
-            <Button style={{ margin: "2px" }}>Edytuj</Button>
-            <Button secondary style={{ margin: "2px" }}>
-              Usuń
-            </Button>
-            <Button.Group primary>
-              <Button primary style={{ margin: "2px 0 2px 30px" }}>
+            <Button.Group primary floated="right">
+              <Button primary>
                 <Icon name="add to calendar" />
                 Nowa wizyta
               </Button>
               <Dropdown
                 floating
-                style={{ margin: "2px" }}
                 onChange={() => {
                   console.info("onc");
                   dispatch(edit("patients", patient._id));
@@ -66,25 +62,119 @@ const PatientDetails: React.FunctionComponent<{}> = () => {
           </Grid.Column>
         </Grid.Row>
       </Grid>
-      <Grid columns={2} stackable>
+      <Grid divided style={{ paddingTop: "0.5em" }}>
         <Grid.Row>
-          <Grid.Column width={7} style={{ margin: "1em 0.5em 3em 0" }}>
-            <Header as="h4">Data urodzenia: {patient.birthDate} </Header>
-            <Header as="h4">Data ostatniej wizyty: (DD/MM/YYYY) </Header>
-            <Header as="h4">Inne informacje:</Header>
+          <Grid.Column width={7} style={{ padding: "0 2em 3em 0" }}>
+            <p>
+              <strong>Data urodzenia: </strong>03.06.1982
+            </p>
+            <p>
+              <strong>Ostatnia wizyta: </strong>24.05.2017
+            </p>
+            <Header as="h5">Inne informacje:</Header>
             <Segment>
               Pellentesque habitant morbi tristique senectus. Quisque at gravida
               mauris. Maecenas aliquam, odio dapibus tempor faucibus, sapien
               ante condimentum est, sed feugiat tortor sem at arcu. Vivamus
               egestas sem eget euismod pellentesque.
             </Segment>
+            <Header as="h5">Ostatnie wizyty:</Header>
+            <Table celled style={{ margin: "0" }}>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Data i godzina</Table.HeaderCell>
+                  <Table.HeaderCell>Notatki</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell width={2}>
+                    <Header as="h5" image>
+                      <Icon name="checked calendar" />
+                      <Header.Content>
+                        02.02.2017
+                        <Header.Subheader>09:00</Header.Subheader>
+                      </Header.Content>
+                    </Header>
+                  </Table.Cell>
+                  <Table.Cell width={4}>
+                    Nulla nisl quam, porttitor in sem vel, feugiat convallis
+                    augue. Fusce efficitur dui ligula, et bibendum dui ultrices
+                    at. Aliquam erat volutpat.
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell width={2}>
+                    <Header as="h5" image>
+                      <Icon name="checked calendar" />
+                      <Header.Content>
+                        04.08.2016
+                        <Header.Subheader>11:00</Header.Subheader>
+                      </Header.Content>
+                    </Header>
+                  </Table.Cell>
+                  <Table.Cell width={4}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque id sem non tellus feugiat dignissim eu vitae
+                    nisl.
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell width={2}>
+                    <Header as="h5" image>
+                      <Icon name="checked calendar" />
+                      <Header.Content>
+                        11.05.2016
+                        <Header.Subheader>14:15</Header.Subheader>
+                      </Header.Content>
+                    </Header>
+                  </Table.Cell>
+                  <Table.Cell width={4}>
+                    Nullam semper mattis tortor id efficitur.
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell width={2}>
+                    <Header as="h5" image>
+                      <Icon name="checked calendar" />
+                      <Header.Content>
+                        25.06.2015
+                        <Header.Subheader>10:00</Header.Subheader>
+                      </Header.Content>
+                    </Header>
+                  </Table.Cell>
+                  <Table.Cell width={4}>
+                    Nam ut egestas orci. Donec cursus massa lacinia ligula
+                    varius, nec porta odio dapibus.
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell width={2}>
+                    <Header as="h5" image>
+                      <Icon name="checked calendar" />
+                      <Header.Content>
+                        04.12.2014
+                        <Header.Subheader>12:30</Header.Subheader>
+                      </Header.Content>
+                    </Header>
+                  </Table.Cell>
+                  <Table.Cell width={4}>
+                    Ut vel mollis leo. Suspendisse consectetur neque sed gravida
+                    ornare. Fusce ipsum urna, ornare vel viverra et, blandit et
+                    velit.
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
+            <Button size="small" attached="bottom" content="Starsze wizyty" />
           </Grid.Column>
-          <Grid.Column width={8} style={{ margin: "1em 0 3em 0.5em" }}>
-            <Header as="h4">
-              Ostatnia wizyta (data i godzina): (DD/MM/YYYY, 10:30)
-            </Header>
-            <Header as="h5">Skan z ostatniej wizyty:</Header>
-            <p>( tutaj skan )</p>
+          <Grid.Column width={9} style={{ padding: "0 0 3em 2em" }}>
+            <Header as="h4">Ostatnie badanie:</Header>
+            <Header.Subheader>02.02.2017, 09:00</Header.Subheader>
+            <Segment>
+              <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+            </Segment>
           </Grid.Column>
         </Grid.Row>
       </Grid>
