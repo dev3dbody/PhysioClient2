46ba4712ddf9e8b158a6784c6253fca5b696dea4
lgielar <lukasz.gielar@grandparade.co.uk>
2019-07-31 20:19:47 +0200

    Manipulacja widokiem skanu - CLI fix

--- e2e/patients.js
+++ e2e/patients.js
@@ -24,9 +24,7 @@ describe("patients", function() {
       for (const field of fields) {
         values[field] = casual[field];
         // Form.Input produces div with input inside
-        await $(
-          `[data-cy="${field}"]${field !== "description" ? " input" : ""}`
-        ).setValue(values[field]);
+        await $(`[data-cy="${field}"] input`).setValue(values[field]);
       }
 
       // wait optionally to visually check if form is filled ok
