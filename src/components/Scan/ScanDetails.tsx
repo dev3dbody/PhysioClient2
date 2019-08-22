4ca955a7a2c4d0104202f3a26b4adb770c7d8e1a
Jakub Wietrzyk <jakub@wietrzyk.com>
2019-08-22 17:56:41 +0200

    player init with null data

--- src/components/Scan/ScanDetails.tsx
+++ src/components/Scan/ScanDetails.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import _ from "lodash";
 import { Container } from "semantic-ui-react";
 import { useSelector } from "react-redux";
 import { getCurrentScan } from "../../redux/reducers";
 import MeshViewer from "../MeshViewer/MeshViewer";
 
-const StanDetails: React.FunctionComponent<{}> = () => {
+const StanDetails = () => {
+  const [ready, setReady] = useState(false);
   const scan = useSelector(getCurrentScan);
   if (!scan) {
     return null;
   }
+  _.defer(() => setReady(true));
   return (
     <Container>
-      <MeshViewer data={scan.mesh} />
+      <MeshViewer data={ready ? scan.mesh : null} />
     </Container>
   );
 };
